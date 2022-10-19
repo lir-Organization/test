fb04b38faa32bf18390ad00d586341f5b2e48556
Lir Shindelman <lshindelman@paloaltonetworks.com>
2022-10-19 10:18:20 +0000

    cicd_test.tf created online with Bitbucket

---
+++ cicd_test.tf
@@ -0,0 +1,10 @@
+resource "azurerm_managed_disk" "source" {
+  encryption_settings {
+    enabled = false
+  }
+  create_option        = ""
+  location             = ""
+  name                 = ""
+  resource_group_name  = "foo"
+  storage_account_type = ""
+}
